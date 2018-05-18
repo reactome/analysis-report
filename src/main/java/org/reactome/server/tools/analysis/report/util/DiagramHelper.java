ba90a5e8741e37c42078ca807d5760a5fa80f935
Pascual Lorente <pasculorente@gmail.com>
2018-05-18 15:26:53 +0100

    removed unnecessary code

--- src/main/java/org/reactome/server/tools/analysis/report/util/DiagramHelper.java
+++ src/main/java/org/reactome/server/tools/analysis/report/util/DiagramHelper.java
@@ -1,6 +1,7 @@
 package org.reactome.server.tools.analysis.report.util;
 
-import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.pdf.xobject.PdfFormXObject;
+import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.property.HorizontalAlignment;
 import org.reactome.server.analysis.core.result.AnalysisStoredResult;
@@ -17,11 +18,7 @@ import org.reactome.server.tools.diagram.exporter.raster.profiles.ColorProfiles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
 
 /**
  * Help to create the diagram image by invoking the Reactome {@link
@@ -32,66 +29,29 @@ import java.util.Collections;
 public class DiagramHelper {
 	private static final double IMAGE_SCALE = 2;  // 1=keep original ppp
 	private static final int MIN_QUALITY = 3;
+	private static final Logger logger = LoggerFactory.getLogger(DiagramHelper.class.getName());
 	private static RasterExporter exporter;
 	private static DiagramService diagramService = ReactomeGraphCore.getService(DiagramService.class);
 	private static String diagramProfile;
 	private static String analysisProfile;
-	private static final Logger logger = LoggerFactory.getLogger(DiagramHelper.class.getName());
-	/**
-	 * create diagram image by using the RasterExporter{@see RasterExporter}.
-	 *
-	 * @param stId stable identifier of the diagram.
-	 * @param asr  {@see AnalysisStoredResult} contains the diagram analysis
-	 *             overlay information.
-	 *
-	 * @return diagram.
-	 */
-	public static Image getDiagram(String stId, AnalysisStoredResult asr, String resource, double pageWidth, double pageHeight) {
+
+	public static void insertDiagram(String stId, AnalysisStoredResult result, String resource, Document document) {
 		final DiagramResult diagramResult = getDiagramResult(stId);
-		final RasterArgs args = new RasterArgs(diagramResult.getDiagramStId(), "png");
+		final RasterArgs args = new RasterArgs(diagramResult.getDiagramStId(), "pdf");
 		args.setSelected(diagramResult.getEvents());
 		args.setWriteTitle(false);
 		args.setResource(resource);
 		args.setProfiles(new ColorProfiles(diagramProfile, analysisProfile, null));
-
-		// 1 ask for an image larger than the original diagram -> better quality
-//		final Integer width = diagramResult.getWidth();
-//		final double desiredWidth = Math.min(width, pageWidth) * IMAGE_SCALE;
-//		final double scale = desiredWidth / width;
-//		int quality = Math.max(toQuality(scale), MIN_QUALITY);
-		args.setQuality(10);
-
 		try {
-			final BufferedImage image = exporter.export(args, asr);
-			final Image fImage = new Image(ImageDataFactory.create(image, Color.WHITE));
-			fImage.setHorizontalAlignment(HorizontalAlignment.CENTER);
-			// 2 downscale to fit page
-			final Double factor = Collections.min(Arrays.asList(
-					1. / IMAGE_SCALE,
-					pageWidth / image.getWidth(),
-					pageHeight / image.getHeight())) - 0.01f;
-			fImage.scale(factor.floatValue(), factor.floatValue());
-			return fImage;
-		} catch (DiagramJsonNotFoundException | AnalysisException | DiagramJsonDeserializationException | EhldException | IOException e) {
-			logger.warn("Couldn't generate diagram: " + stId, e);
-		} catch (RuntimeException e){
-			logger.warn("Error found for: " + stId, e);
+			final Document imagePdf = exporter.exportToPdf(args, result);
+			final PdfFormXObject object = imagePdf.getPdfDocument().getFirstPage().copyAsFormXObject(document.getPdfDocument());
+			final float wi = document.getPdfDocument().getLastPage().getPageSize().getWidth() - document.getLeftMargin() - document.getRightMargin() - 0.1f;  // avoid image too large
+			final float he = 0.5f * document.getPdfDocument().getLastPage().getPageSize().getHeight() - document.getTopMargin() - document.getBottomMargin();
+			document.add(new Image(object).scaleToFit(wi, he).setHorizontalAlignment(HorizontalAlignment.CENTER));
+			document.flush();
+		} catch (AnalysisException | EhldException | DiagramJsonNotFoundException | DiagramJsonDeserializationException | IOException e) {
+			logger.error("Couldn't insert diagram " + stId, e);
 		}
-		return null;
-	}
-
-	private static int toQuality(double scale) {
-		if (scale <= 0.1) return 1;
-		if (scale >= 3) return 10;
-		double quality;
-		if (scale <= 1) {
-			quality = interpolate(scale, 0.1, 1, 1, 5);
-		} else quality = interpolate(scale, 1, 3, 5, 10);
-		return Math.toIntExact(Math.round(quality));
-	}
-
-	private static double interpolate(double value, double min, double max, double targetMin, double targetMax) {
-		return (value - min) / (max - min) * (targetMax - targetMin) + targetMin;
 	}
 
 	public static void setPaths(String diagramPath, String ehldPath, String analysisPath, String svgSummary) {
@@ -106,4 +66,5 @@ public class DiagramHelper {
 		DiagramHelper.diagramProfile = diagramProfile;
 		DiagramHelper.analysisProfile = analysisProfile;
 	}
+
 }
--- src/main/java/org/reactome/server/tools/analysis/report/util/FireworksHelper.java
+++ src/main/java/org/reactome/server/tools/analysis/report/util/FireworksHelper.java
@@ -1,12 +1,16 @@
 package org.reactome.server.tools.analysis.report.util;
 
-import org.reactome.server.analysis.core.result.AnalysisStoredResult;
+import com.itextpdf.kernel.pdf.xobject.PdfFormXObject;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.property.HorizontalAlignment;
 import org.reactome.server.tools.analysis.report.AnalysisData;
 import org.reactome.server.tools.fireworks.exporter.FireworksExporter;
 import org.reactome.server.tools.fireworks.exporter.common.analysis.exception.AnalysisServerError;
 import org.reactome.server.tools.fireworks.exporter.common.api.FireworkArgs;
+import org.slf4j.LoggerFactory;
 
-import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * Help to create the fireworks image by invoking the Reactome {@link
@@ -20,23 +24,22 @@ public class FireworksHelper {
 	private static FireworksExporter exporter;
 	private static String profile;
 
-	/**
-	 * get the fireworks image from {@link FireworksExporter} by given analysis
-	 * token.
-	 *
-	 * @param analysisData {@link AnalysisStoredResult}.
-	 *
-	 * @return fireworks.
-	 *
-	 * @see FireworksExporter
-	 */
-	public static BufferedImage getFireworks(AnalysisData analysisData) throws AnalysisServerError {
-		final FireworkArgs args = new FireworkArgs(analysisData.getSpecies().replace(" ", "_"), "png");
+	public static void insertFireworks(Document document, AnalysisData data) throws AnalysisServerError {
+		final FireworkArgs args = new FireworkArgs(data.getSpecies().replace(" ", "_"), "png");
 		args.setFactor(QUALITY);
 		args.setWriteTitle(false);
 		args.setProfile(profile);
-//		args.setProfile("Copper plus");
-		return exporter.renderRaster(args, analysisData.getAnalysisStoredResult());
+		try {
+			final Document fireworks = exporter.renderPdf(args, data.getAnalysisStoredResult());
+			final PdfFormXObject object = fireworks.getPdfDocument().getFirstPage().copyAsFormXObject(document.getPdfDocument());
+			final float wi = document.getPdfDocument().getLastPage().getPageSize().getWidth() - document.getLeftMargin() - document.getRightMargin() - 0.1f;  // avoid image too large
+			final float he = 0.5f * document.getPdfDocument().getLastPage().getPageSize().getHeight() - document.getTopMargin() - document.getBottomMargin();
+			document.add(new Image(object).scaleToFit(wi, he).setHorizontalAlignment(HorizontalAlignment.CENTER));
+			document.flush();
+		} catch (IOException e) {
+			LoggerFactory.getLogger(FireworksHelper.class).error("Couldn't insert fireworks", e);
+		}
+
 	}
 
 	public static void setPaths(String fireworksPath, String analysisPath) {
