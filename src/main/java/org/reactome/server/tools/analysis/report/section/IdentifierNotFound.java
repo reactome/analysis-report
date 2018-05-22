3fa80c53847144b84e3551856a5e8b46831d2300
Pascual Lorente <pasculorente@gmail.com>
2018-05-22 10:29:22 +0100

    sorted identifiers not found

--- src/main/java/org/reactome/server/tools/analysis/report/section/IdentifierNotFound.java
+++ src/main/java/org/reactome/server/tools/analysis/report/section/IdentifierNotFound.java
@@ -8,6 +8,10 @@ import org.reactome.server.analysis.core.result.model.IdentifierSummary;
 import org.reactome.server.tools.analysis.report.AnalysisData;
 import org.reactome.server.tools.analysis.report.style.PdfProfile;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author Chuan-Deng dengchuanbio@gmail.com
  */
@@ -21,12 +25,16 @@ public class IdentifierNotFound implements Section {
 		table.useAllAvailableWidth();
 		int i = 0;
 		int row = 0;
-		for (IdentifierSummary summary : analysisData.getAnalysisStoredResult().getNotFoundIdentifiers()) {
+		final List<IdentifierSummary> sorted = analysisData.getAnalysisStoredResult().getNotFoundIdentifiers().stream()
+				.sorted(Comparator.comparing(IdentifierSummary::getId))
+				.distinct()
+				.collect(Collectors.toList());
+		for (IdentifierSummary summary : sorted) {
 			row = i / 8;
-			table.addCell(profile.getBodyCell(summary.getId(), row));
+			table.addCell(profile.getBodyCell(summary.getId(), row).setMargin(1.5f));
 			i += 1;
 		}
-		final int n = 8 - analysisData.getAnalysisStoredResult().getNotFoundIdentifiers().size() % 8;
+		final int n = 8 - sorted.size() % 8;
 		for (int j = 0; j < n; j++) table.addCell(profile.getBodyCell("", row));
 		document.add(table);
 	}
